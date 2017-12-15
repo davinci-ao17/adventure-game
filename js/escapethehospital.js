307ca99a21b39c7b6b4c31d3056a9bf697078c2c
Ludo de Vries <99044419@mydavinci.nl>
2017-12-15 09:27:25 +0100

    af.

--- js/escapethehospital.js
+++ js/escapethehospital.js
@@ -1,13 +1,25 @@
 var hasDopper = false;
 var hasKey = false;
 var hasKey1 = false;
+var receptie = false;
+var hasLadderExtension = false;
+var hasShowered = false;
+var hasLadder = false;
+var firstmessage = false;
+var hasKey3 = false;
+
+var deaths1;
+
+var x = document.getElementById("strange");
+
 var death = new Audio("music/classic_hurt.ogg");
 var sure1 = new Audio("music/sure.mp3");
 var glass = new Audio("music/glass1.ogg");
 var fall = new Audio("music/elytra_loop.ogg");
 var locked = new Audio("music/locked.mp3");
 var rollover = new Audio("music/rollover1.wav");
-var strange = new Audio("music/Strangeness.mp3");
+var pop = new Audio("music/pop.ogg");
+
 var dopper = document.getElementById("dopper");
 var afbeelding2 = document.getElementById("afbeelding2");
 var afbeelding3 = document.getElementById("afbeelding3");
@@ -20,18 +32,24 @@ var nokey = document.getElementById("noKey");
 var nod = document.getElementById("noD");
 var voorkant1 = document.getElementById("voorkant1");
 var key = document.getElementById("key");
-var desc = document.getElementById("desc");
 var deur = document.getElementById("deur1");
 var dubbeleladder = document.getElementById("dubbeleladder1");
+var extensionLadder = document.getElementById("extensionLadder1");
 var raam = document.getElementById("raam1");
 var nokeyd = document.getElementById("noKeyd");
 var afbeelding6D = document.getElementById("afbeelding6d");
-var receptie = false;
 var schuur = document.getElementById("schuur");
-var hasLadder = false;
+var kamer1 = document.getElementById("kamer");
+var bathroom1 = document.getElementById("bathroom");
+var keyBox = document.getElementById("keyBox1");
+var tv = document.getElementById("tv");
+var laptop = document.getElementById("laptop");
+var bed = document.getElementById("bed");
+
 var key1 = document.getElementById("key1");
 var titles = document.getElementById("titles");
-var firstmessage = false;
+var desc = document.getElementById("desc");
+var key3a = document.getElementById("key3");
 
 function hideAll() {
     afbeelding1.style.display = "none";
@@ -46,6 +64,8 @@ function hideAll() {
     nokeyd.style.display = "none";
     afbeelding6D.style.display = "none";
     schuur.style.display = "none";
+    kamer1.style.display = "none";
+    bathroom1.style.display = "none";
 
     ingang1.style.display = "none";
     voorkant1.style.display = "none";
@@ -70,24 +90,17 @@ function begin() {
     if (!firstmessage) {
         desc.innerHTML = "Mmmm I don't see anyone perhaps there is someone<br> somewhere else.";
         firstmessage = true;
-  } else {
+    } else {
         desc.innerHTML = "Perhaps I can look at the Reception.";
-  }
+    }
     titles.innerHTML = "Hospitalbed";
     afbeelding1.style.display = "inline";
 
     console.log("Hospitalbed");
-
-    if (strange.paused) {
-        strange.currentTime = 0;
-        strange.play();
-    } else {
-        strange.pause();
-    }
 }
 
 function info() {
-  desc.innerHTML = "Finish the game by clicking on the screen.";
+    desc.innerHTML = "Finish the game by clicking on the screen.";
 }
 
 function afbeelding2f() {
@@ -128,6 +141,7 @@ function afbeelding4f() {
 
 function afbeelding5df() {
     hideAll();
+    afbeelding5d.style.display = "inline";
 }
 
 function afbeelding5f() {
@@ -143,6 +157,7 @@ function keyD() {
         hideAll();
         afbeelding5d.style.display = "inline";
         key.style.display = "none";
+        titles.innerHTML = "Reception";
 
     } else if (hasDopper && !hasKey) {
         //heeft wel dopper maar geen key
@@ -154,9 +169,11 @@ function keyD() {
         console.log("Reception");
         afbeelding5.style.display = "inline";
         key.style.display = "none";
+        titles.innerHTML = "Reception";
     } else if (hasDopper && hasKey && hasKey1) {
-      hideAll();
-      afbeelding5.style.display = "inline";
+        hideAll();
+        afbeelding5.style.display = "inline";
+        titles.innerHTML = "Reception";
       //heeft dopper sleutel en sleutel
     } else {
         //heeft geen items
@@ -174,10 +191,10 @@ function noKeyf() {
 function keyf() {
     hideAll();
     if (hasKey1) {
-    nokey.style.display = "inline";
+        nokey.style.display = "inline";
     } else {
-    afbeelding6.style.display = "inline";
-    desc.innerHTML = "The key of the entrance.";
+        afbeelding6.style.display = "inline";
+        desc.innerHTML = "The key of the entrance.";
     }
 }
 
@@ -202,45 +219,173 @@ function sure() {
     sure1.play();
 }
 
-function noD() {
+function noD1() {
     hideAll();
+    noD.style.display = "inline";
 }
 
 function ingang() {
     hideAll();
     ingang1.style.display = "inline";
     key1.style.display = "none";
+    titles.innerHTML = "Entrance";
+    desc.innerHTML = "Click on the top left to go home.";
+    console.log("Entrance");
 }
 
 function voorkant() {
-    if (hasKey1) {
-    hideAll();
-    voorkant1.style.display = "inline";
-    deur.style.display = "inline";
+        if (hasKey3) {
+        voorkant1.style.display = "inline";
+        desc.innerHTML = "<li><ul onclick='unlockRoom();'>Click here to unlock the door and go to your room</ul></li>";
+        kamer1.style.display = "none";
+    } else if (hasKey1) {
+        hideAll();
+        voorkant1.style.display = "inline";
+        deur.style.display = "inline";
+        raam.style.display = "inline";
+        titles.innerHTML = "House";
+        console.log("House");
     }
-    if (!hasLadder) {
-      dubbeleladder.style.display = "none";
+    else if(!hasLadder && hasDopper && hasKey1) {
+        dubbeleladder.style.display = "none";
+        titles.innerHTML = "House";
+        console.log("House");
+    } else if(!hasLadderExtension && hasDopper && hasKey1) {
+        extensionLadder.style.display = "none";
+        titles.innerHTML = "House";
+        console.log("House");
+      } else if (hasKey3) {
+        desc.innerHTML = "<li><ul onclick='unlockRoom();'>Click here to unlock the door and go to your room</ul></li>"
     } else {
-      desc.innerHTML = "You don't have the key!";
+        desc.innerHTML = "You don't have the key!";
     }
+}
+
+function unlockRoom() {
+    titles.innerHTML = "Your Room";
+    tv.style.display = "inline";
+    key3a.style.display = "none"
+    desc.innerHTML = "<li><ul onclick='useTv();'>Click here to Watch tv</ul><ul onclick='useLaptop();'>Click here to use your laptop</ul><ul onclick='useBed();'>Click here to sleep</ul></li>"
+}
+
+function useTv() {
+      deaths1 = setTimeout(function () {
+        alert("You died because you get too tired"); window.location.reload(true);
+    }, 10000);
+    desc.innerHTML = "<li><ul onclick='useBed();'>Click here to sleep</ul></li>";
+
+}
+
+function useLaptop() {
+    laptop.style.display = "inline";
+        deaths1 = setTimeout(function () {
+        alert("You died because you get too tired"); window.location.reload(true);
+    }, 10000);
+    desc.innerHTML = "<li><ul onclick='useBed();'>Click here to sleep</ul></li>";
+}
+
+function useBed() {
+  bed.style.display = "inline";
+  stopDeath();
+  desc.innerHTML = "";
+  titles.innerHTML = "";
+  alert("You finished click on ok to play again."); window.location.reload(true);
+}
+
+function stopDeath() {
+    clearTimeout(deaths1);
 }
 
 function schuur1() {
     hideAll();
     schuur.style.display = "inline";
+    titles.innerHTML = "Shed";
+    console.log("Shed");
     if (!hasLadder) {
-    dubbeleladder.style.display = "inline";
-  }
+        dubbeleladder.style.display = "inline";
+        extensionLadder.style.display = "inline";
+    }
 }
 
-function raam1() {
-  dubbeleladder.style.display = "inline";
+function raam2() {
+    if (hasLadder && hasLadderExtension) {
+        desc.innerHTML = "<li><ul onclick='placeDubbele();'>Doubleladder</ul><ul onclick='placeExtension();'>Extension ladder</ul></li>";
+    }
+}
+
+function placeDubbele() {
+    dubbeleladder.style.display = "inline";
+    death.play();
+    setTimeout(function () {
+        alert("The ladder has collapsed"); window.location.reload(true);
+    }, 500);
+}
+
+function placeExtension() {
+    hideAll();
+    kamer1.style.display = "inline";
+    desc.innerHTML = "<li><ul>I can take a shower here or go directly to the key box.</ul> <ul onclick='openKeyBox();'>Find the key of your house</ul></li>";
+    extensionLadder.style.display = "none";
+    titles.innerHTML = "Room";
+    console.log("Room");
+}
+
+function takeShower() {
+    if (hasShowered === true) {
+        desc.innerHTML = "<li><ul>You have already have taken a shower.</ul> <ul onclick='openKeyBox();'>Find the key of your house</ul></li>";
+    } else {
+        bathroom1.style.display = "inline";
+        hasShowered = true;
+        desc.innerHTML = "Showering..."
+        titles.innerHTML = "Bathroom";
+        console.log("Shower");
+    setTimeout(function () {
+            kamer1.style.display = "inline";
+            bathroom1.style.display = "none";
+            desc.innerHTML = "<ul onclick='openKeyBox();'>Find the key of your house</ul></li>"
+            titles.innerHTML = "Room";
+        }, 5000);
+    }
+}
+
+function openKeyBox() {
+    keyBox.style.display = "inline";
+    desc.innerHTML = "<li><ul>Find the key of your house</ul><ul onclick='pickupKey3();'></ul></li>";
+    if (hasShowered === true) {
+        key3a.style.display = "inline";
+        titles.innerHTML = "Keybox";
+        console.log("Keyboy");
+    } else if (hasKey3 === true) {
+        desc.innerHTML = "You already found your house key.";
+    } else {
+        desc.innerHTML = "You are too tired to see your house key.";
+        titles.innerHTML = "Keybox";
+        console.log("Keybox");
+    setTimeout(function () {
+            kamer1.style.display = "inline";
+            keyBox.style.display = "none";
+            titles.innerHTML = "Room";
+        }, 5000);
+    }
+}
+
+function pickupKey3() {
+    hasKey3 = true;
+    key3a.style.display = "inline";
+    key3a.style.margin = "250px 1530px";
+    pop.play();
+    kamer1.style.display = "inline";
+    keyBox.style.display = "none";
+    titles.innerHTML = "Room";
+    console.log("Room");
+    desc.innerHTML = "<li><ul onclick='voorkant();'>Click here to go back to the street.</ul></li>";
 }
 
 function pickupKey() {
     hasKey = true;
     key.style.margin = "150px 1530px";
     desc.innerHTML = "Perhaps I need this key to access another room.";
+    pop.play();
 }
 
 function pickupLadder() {
@@ -248,22 +393,32 @@ function pickupLadder() {
     dubbeleladder.style.margin = "150px 1515px";
     dubbeleladder.style.width = "80px";
     dubbeleladder.style.height = "80px";
-    }
+    pop.play();
+}
+
+function pickupLadderExtension() {
+    hasLadderExtension = true;
+    extensionLadder.style.margin = "250px 1515px";
+    extensionLadder.style.width = "80px";
+    extensionLadder.style.height = "80px";
+    pop.play();
+}
 
 function pickupKey1() {
     hasKey1 = true;
     afbeelding6.style.display = "inline";
     key1.style.display = "inline";
     key1.style.margin = "130px 1490px";
-
     noKeyf();
+    pop.play();
 }
 
 function dopper1() {
     if (!hasDopper) {
         hasDopper = true;
-    dopper.style.margin = "20px 1530px";
-    desc.innerHTML = "Mmm delicious water";
+        dopper.style.margin = "20px 1530px";
+        desc.innerHTML = "Mmm delicious water";
+        pop.play();
     }
 }
 
@@ -277,6 +432,12 @@ function stopSound() {
     rollover.currentTime = 0;
 }
 
-function strange2() {
-    strange1.play();
-}
+function enableLoop() { 
+    x.loop = true;
+    x.load();
+} 
+
+function disableLoop() { 
+    x.loop = false;
+    x.load();
+} 
